bee2d8821a4fa67947a6f52024d5f768e89a5436
KnifeParty12 <flashfunk01@gmail.com>
2019-06-06 10:23:00 +0530

    Added New Game Functionality
    Signed-off-by: KnifeParty12 <flashfunk01@gmail.com>

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 var scores, roundScore,activePlayer;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
+function init () {
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
 
-//document.querySelector('#current-' + activePlayer ).textContent = dice;
-// document.querySelector('#current-' + activePlayer ).innerHTML = '<em>' + dice +  '</em>';
+    document.querySelector('.dice').style.display = 'none';
 
-document.querySelector('.dice').style.display = 'none';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+}
 
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
-
+init();
 
 document.querySelector('.btn-roll').addEventListener('click', ()=>{
     //Random Number
@@ -67,3 +74,6 @@ function nextPlayer() {
     document.querySelector('.dice').style.display = 'none';
 
 }
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
